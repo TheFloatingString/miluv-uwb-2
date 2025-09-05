957aebe65137ed747711ccdc2da32e59fe9c7464
Laurence Liang <laurence.w.liang@gmail.com>
2025-09-04 23:40:23 -0400

    refactor: ewine

--- scripts/run_ewine_vs_miluv.py
+++ scripts/run_ewine_vs_miluv.py
@@ -1,11 +1,11 @@
 import argparse
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from tabpfn import TabPFNClassifier
 
 SOURCE_DATA = "EWINE_UWB_DATASET_PARTS"
+
 
 def main():
     if SOURCE_DATA == "EWINE_TAG_ROOM":
@@ -13,15 +13,18 @@ def main():
         print(ewine_df.head())
 
         # get last 1,016 columns as X_data
-        X_data = ewine_df.iloc[:, 24:-1]
+        X_data = ewine_df.iloc[:, -501:-1]
         print(X_data.head())
         print(X_data.shape)
 
-        y_data = ewine_df.iloc[:,5]
+        y_data = ewine_df.iloc[:, 5]
         print(y_data.head())
         print(y_data.unique())
+        print(y_data.value_counts())
 
-        X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X_data, y_data, test_size=0.2, random_state=42
+        )
         print(X_train.shape)
         print(X_test.shape)
         print(y_train.shape)
@@ -33,10 +36,13 @@ def main():
         print(ewine_df.iloc[:, 0].head())
 
         X_data = ewine_df.iloc[:, 15:].values
-        X_data = X_data[0:500,-500:]
-        y_data = ewine_df.iloc[0:500, 0].values
+        X_data = X_data[:, -500:]
+        y_data = ewine_df.iloc[:, 0]
+        print(y_data.value_counts())
 
-        X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X_data, y_data, test_size=0.2, random_state=42
+        )
         print(X_train.shape)
         print(X_test.shape)
         print(y_train.shape)
@@ -47,6 +53,7 @@ def main():
     y_pred = clf.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
     print(f"Accuracy: {acc}")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
