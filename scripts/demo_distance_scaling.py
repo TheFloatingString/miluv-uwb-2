c33d9a14138ae48bd22d218b6033ee1d3f2cdf04
Laurence Liang <laurence.w.liang@gmail.com>
2025-09-09 00:02:05 -0400

    fix: import

--- scripts/demo_distance_scaling.py
+++ scripts/demo_distance_scaling.py
@@ -4,6 +4,7 @@ import argparse
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from tabpfn import TabPFNClassifier
 import tqdm
 import numpy as np
 from sklearn.model_selection import train_test_split
